1df12fbbb6b3d9d6a2448844b2be64e2ca5f53f8
jahusayuri <josh.yuri.requioma@gmail.com>
2018-03-12 21:32:07 +0800

    fixed addstock_handler.php; can now update number of stocks

--- addstock_handler.php
+++ addstock_handler.php
@@ -2,19 +2,34 @@
 session_start();
 include("connect.php");
 $called_id = $_SESSION['product_id'];
+$supplier_username = $_SESSION['username'];
 
 if(isset($_POST['addStock']));
-	{
-		// echo $called_id;
-		$stock = $_POST['stock'];	
-		$sql = "UPDATE product(stock)
-				SET('$stock')";
+	{		
+		//echo $supplier_username;
+		//echo "</br>";
+		//echo $called_id;
+		//echo "</br>";
+
+		//Get supplier ID from supplier table
+		$sql_id = "SELECT supplier_id FROM supplier WHERE username = '$supplier_username' ";
+		$result_id = mysqli_query($db,$sql_id);
+
+		//Extract id from the supplier and set to variable
+		$row = mysqli_fetch_assoc($result_id);
+		$retrieved_id = $row["supplier_id"];
+		
+		$stock = $_POST['stock'];
+		//echo "</br>";
+		//echo $stock;
+			
+		$sql = "UPDATE product
+				SET stock = '$stock'
+				WHERE supplier_id = '$retrieved_id'";
 		
 		$result=mysqli_query($db,$sql);
-
-				// echo $sql;
+		//var_dump($result);
 		
-
 				
 	}
 
