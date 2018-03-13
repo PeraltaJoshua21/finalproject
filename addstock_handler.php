0b0f3a38b679cb17db33ddfd60e56784c096755c
jahusayuri <josh.yuri.requioma@gmail.com>
2018-03-13 20:02:18 +0800

    updated the stock part to add previous

--- addstock_handler.php
+++ addstock_handler.php
@@ -22,19 +22,36 @@ if(isset($_POST['addStock']));
 		$stock = $_POST['stock'];
 		//echo "</br>";
 		//echo $stock;
-			
+		//echo "</br>";
+
+		//Retrieve number of Stock
+		$sql_get_stock = "SELECT stock FROM product WHERE supplier_id = '$retrieved_id' AND prod_id = '$called_id'";
+		$retrieved_stock = mysqli_query($db,$sql_get_stock);
+		
+		$row_stock = mysqli_fetch_assoc($retrieved_stock);
+		$db_stock = $row_stock["stock"];
+		//echo $db_stock;
+		//echo "</br>";
+
+		$stock_array = array($stock, $db_stock);
+		$sum_stock_number = (int)array_sum($stock_array);
+		//echo $sum_stock_number;
+		//echo "</br>";
+
 		$sql = "UPDATE product
-				SET stock = '$stock'
-				WHERE supplier_id = '$retrieved_id'";
+				SET stock = '$sum_stock_number'
+				WHERE supplier_id = '$retrieved_id' AND prod_id = '$called_id'";
 		
 		$result=mysqli_query($db,$sql);
+		//echo $result;
+		//echo "</br>";
 		//var_dump($result);
 		
 				
 	}
 
 	echo "<script>alert('Stocks added!')
-	 		window.location='supplierhome.php'</script>";
+			window.location='supplierhome.php'</script>";
 
 
 ?>
